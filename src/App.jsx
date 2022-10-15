2ef3e6b2593f1ae11725284d88efbd23ddaf81e4
BrokenFridge <103411852+Arq091@users.noreply.github.com>
2022-10-15 08:57:27 +0700

    removed sound, cant select emojis now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react"
 import supabase from "./client"
-import sound from "./sound/sfx.mp3"
 
 export default function App() {
   const options = ["rock", "paper", "scissors"]
@@ -59,14 +58,7 @@ export default function App() {
     return item
   }
 
-  const playsound = () => {
-    const audio = new Audio(sound)
-    audio.currentTime = 0
-    audio.play()
-  }
-
   async function handleOption(option) {
-    playsound()
     const botOption = getRandomItem(options)
     setBotChose(botOption)
     setRoundsPlayed(roundsPlayed + 1)
